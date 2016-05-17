00a2b5010fea5610b26182c50604177c45971e21
a3zzat <viva.amego@gmail.com>
2016-05-17 15:11:43 +0200

    Adding initial audio project

---
+++ audio/audio_2/project_2.sdk/audio/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../audio_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../audio_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../audio_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../audio_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../audio_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../audio_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../audio_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../audio_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
