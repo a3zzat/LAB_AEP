c30a1123036e2f476f2d01a99e58ab9c9d3c3c3a
a3zzat <viva.amego@gmail.com>
2016-06-02 22:30:06 +0200

    start with ref video project

---
+++ Integrated_redo_/hdl/Projects/fmc_imageon_gs/ZEDBOARD/fmc_imageon_gs.hw/webtalk/labtool_webtalk.tcl
@@ -0,0 +1,28 @@
+webtalk_init -webtalk_dir E:/projects/xilinix/master_repo/LAB_AEP/Video_redo_/hdl/Projects/fmc_imageon_gs/ZEDBOARD/fmc_imageon_gs.hw/webtalk/
+webtalk_register_client -client project
+webtalk_add_data -client project -key date_generated -value "Thu Jun 02 21:57:37 2016" -context "software_version_and_target_device"
+webtalk_add_data -client project -key product_version -value "Vivado v2015.4 (64-bit)" -context "software_version_and_target_device"
+webtalk_add_data -client project -key build_version -value "1412921" -context "software_version_and_target_device"
+webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
+webtalk_add_data -client project -key registration_id -value "211191233_0_0_466" -context "software_version_and_target_device"
+webtalk_add_data -client project -key tool_flow -value "labtool" -context "software_version_and_target_device"
+webtalk_add_data -client project -key beta -value "FALSE" -context "software_version_and_target_device"
+webtalk_add_data -client project -key route_design -value "FALSE" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_family -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "eb8b8473-6354-4e5d-a023-bd024f5cc1da" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_id -value "c8303813-b2c9-4a92-b3d6-1f1f03f3085c" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "2" -context "software_version_and_target_device"
+webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
+webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
+webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i3-4010U CPU @ 1.70GHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "1696 MHz" -context "user_environment"
+webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
+webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
+webtalk_register_client -client labtool
+webtalk_add_data -client labtool -key pgmcnt -value "00:00:00" -context "labtool\\usage"
+webtalk_add_data -client labtool -key cable -value "" -context "labtool\\usage"
+webtalk_transmit -clientid 3651760918 -regid "211191233_0_0_466" -xml E:/projects/xilinix/master_repo/LAB_AEP/Video_redo_/hdl/Projects/fmc_imageon_gs/ZEDBOARD/fmc_imageon_gs.hw/webtalk/usage_statistics_ext_labtool.xml -html E:/projects/xilinix/master_repo/LAB_AEP/Video_redo_/hdl/Projects/fmc_imageon_gs/ZEDBOARD/fmc_imageon_gs.hw/webtalk/usage_statistics_ext_labtool.html -wdm E:/projects/xilinix/master_repo/LAB_AEP/Video_redo_/hdl/Projects/fmc_imageon_gs/ZEDBOARD/fmc_imageon_gs.hw/webtalk/usage_statistics_ext_labtool.wdm -intro "<H3>LABTOOL Usage Report</H3><BR>"
+webtalk_terminate
