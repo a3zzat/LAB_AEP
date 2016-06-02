7cb8d890fb80c67aeecfabd15f947c357d65cac7
a3zzat <viva.amego@gmail.com>
2016-06-02 22:08:31 +0200

    adding old integration project

---
+++ Integrated/Integrated_1/Integrated_1.sdk/fmc_imageon_gs_app/Debug/Audio_main.d
@@ -0,0 +1,60 @@
+Audio_main.o Audio_main.d: ../Audio_main.c \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xiicps.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xiicps_hw.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xuartps_hw.h ../Audio.h
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xiicps.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xiicps_hw.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../fmc_imageon_gs_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../Audio.h:
