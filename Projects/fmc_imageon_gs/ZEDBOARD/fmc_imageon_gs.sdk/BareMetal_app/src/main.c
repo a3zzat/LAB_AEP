c23550c9f06afaf2904522bf769ec3240d9326a5
Benjamin <chidoben@users.noreply.github.com>
2016-06-22 04:51:00 +0200

    Added code for receiving on one port and sending on another port

--- Projects/fmc_imageon_gs/ZEDBOARD/fmc_imageon_gs.sdk/BareMetal_app/src/main.c
+++ Projects/fmc_imageon_gs/ZEDBOARD/fmc_imageon_gs.sdk/BareMetal_app/src/main.c
@@ -1,9 +1,3 @@
-/*
- * main.c
- *
- *  Created on: Aug 1, 2014
- *      Author: 910560
- */
 
 #include "video/demo.h"
 #include "video/avnet_console.h"
@@ -28,17 +22,15 @@ struct netif *udp_netif;
 struct pbuf  *NetPuf;
 int buflen;
 
+void udp_recv_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, u16_t port);
 
 int main()
 {
 
 	xil_printf("--              AudioChip(ADAU1761)                 --\n\r");
-
 	Audio_init();
 
 	xil_printf("--                Gigabit-Ethernet                  --\n\r");
-
-
 	net_init(udp_1,udp_netif);
 	//register the function recv_callback as the call back for incoming functions
 	//this function will be called to process incoming package
@@ -46,9 +38,6 @@ int main()
 
 	
 	xil_printf("--                    FMC-IMAGEON                   --\n\r");
-
-
-
 	pdemo = &demo;
 	demo_init( pdemo );
 
@@ -71,57 +60,84 @@ int main()
 	print_avnet_console_serial_app_header();
 	start_avnet_console_serial_application();
 
-	 buflen = sizeof(AudioPuf);
 
-	xemacif_input(udp_netif);
-	AudioPuf = malloc(sizeof(AudioData_t));
-	if (!AudioPuf) {
+//-------------------------------------------------
+// 	 buflen = sizeof(AudioPuf);
+
+// 	xemacif_input(udp_netif);
+// 	AudioPuf = malloc(sizeof(AudioData_t));
+// 	if (!AudioPuf) {
+// 		xil_printf("error allocating AudioPuf\r\n");
+// 	}
+// 	NetPuf = pbuf_alloc(PBUF_TRANSPORT, buflen, PBUF_POOL);
+// 	if (!NetPuf) {
+// 		xil_printf("error allocating NetPuf\r\n");
+// 	}
+// 	NetPuf->payload = (void *) AudioPuf;
+
+// 	while (1)
+// 	{
+// 		read_play(AudioPuf);
+// //		memcpy(NetPuf->payload, AudioPuf , buflen);
+// 		udp_send(udp_1, NetPuf);
+		
+// 		// needs delay before sending again  think
+// 		if (transfer_avnet_console_serial_data()) {
+// 			break;
+// 		}
+
+
+// 	}
+//-----------------------------------------------
+//My own version of sending and receiving
+
+xemacif_input(udp_netif);
+	if (!(AudioPuf=malloc(sizeof(AudioData_t)))) {
 		xil_printf("error allocating AudioPuf\r\n");
+ 	}
+ 	 buflen = sizeof(AudioPuf);
+NetPuf = pbuf_alloc(PBUF_TRANSPORT, buflen, PBUF_POOL);
+ 	if (!(NetPuf= pbuf_alloc(PBUF_TRANSPORT, buflen, PBUF_POOL))) {
+ 		xil_printf("error allocating NetPuf\r\n");
 	}
-	NetPuf = pbuf_alloc(PBUF_TRANSPORT, buflen, PBUF_POOL);
-	if (!NetPuf) {
-		xil_printf("error allocating NetPuf\r\n");
-	}
-	NetPuf->payload = (void *) AudioPuf;
-
-	while (1)
-	{
-		read_play(AudioPuf);
-//		memcpy(NetPuf->payload, AudioPuf , buflen);
-		udp_send(udp_1, NetPuf);
-		// needs delay before sending again  think
-		if (transfer_avnet_console_serial_data()) {
-			break;
-		}
-
-
-	}
-
+      NetPuf->payload = (void *) AudioPuf;
+      
+      //create a new udp for sending. The other udp already created is used for receiving
+      struct udp_pcb *send_udp = udp_new();
+      
+      //port for sending udp packets
+        u16_t  fwd_port = 5001;
+        
+        //ip address for sending packets. It should be the same as the IP address of the remote board we are 
+        //receiving from
+       struct ip_addr forward_ip;
+       IP4_ADDR(&forward_ip,  192, 168,   1, 11);
+      
+      	while (1)
+ 	{
+ 		//read audio packets
+ 		read_play(AudioPuf);
+ 		//receiving
+ 		udp_recv(udp_1, udp_recv_callback, NULL);
+ 		//sending
+         	udp_sendto(send_udp, NetPuf, &forward_ip, fwd_port); //dest port
+ 	}
+       //Free the buffer
+       pbuf_free(NetPuf);
 	return 0;
 
-//    xil_printf("\r\n\tPress 0-9 to change alpha blending of hdmi/camera layers\r\n");
-//	xil_printf("\r\n\tPress ENTER to restart\r\n\r\n" );
-//	c = getchar();
-
-//	if ( c >= '0' && c <= '9' )
-//	{
-//		camera_alpha = (c - '0') * 28;
-//		hdmi_alpha    = ('9' - c) * 28;
-//	}
-
-//	if ( c == '+' )
-//	{
-//		if ( pdemo->pvita_receiver->uManualTap < 31 )
-//			pdemo->pvita_receiver->uManualTap++;
-//		xil_printf( "\tuManualTap = %d\n\r", pdemo->pvita_receiver->uManualTap );
-//	}
-//	if ( c == '-' )
-//	{
-//		if ( pdemo->pvita_receiver->uManualTap > 0 )
-//			pdemo->pvita_receiver->uManualTap--;
-//		xil_printf( "\tuManualTap = %d\n\r", pdemo->pvita_receiver->uManualTap );
-//	}
-//}
-
-	//return 0;
 }
+
+void udp_recv_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, u16_t port){
+	int i;
+
+	xil_printf("received  bytes at port  %d\n",pcb->local_port);
+      if (p != NULL) {
+      	  printf("Received %d bytes: ", (*p).len);
+      	  
+      //Send the audio bytes to the line out
+      //Send the video bytes to the HDMI output
+            pbuf_free(p);
+      }
+}
+
